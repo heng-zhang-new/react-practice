fc97504df5058237983f395ff0bc6405099a15d7
Heng <zhangheng_fl@163.com>
2022-05-04 22:00:53 +0800

    Login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,54 @@
+import { Card, Form, Input, Button, Checkbox } from 'antd'
+import './index.scss'
+
 function Login() {
-  return <h1>Login</h1>
+  const onFinish = (values) => console.log(values)
+
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img
+          className="login-logo"
+          src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+          alt=""
+        />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} onFI>
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号码格式不对',
+              },
+              { required: true, message: '请输入手机号' },
+            ]}>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              { len: 6, message: '验证码6个字符' },
+              { required: true, message: '请输入验证码' },
+            ]}>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item name="confirm" valuePropName="checked">
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            {/*  渲染 Button 组件为 submit 按钮 */}
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
